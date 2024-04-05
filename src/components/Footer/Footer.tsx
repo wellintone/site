5a5e810a444ececeb5b387103f2d986138011eb5
wellington pecchini <wellington@MBPdiwelington2.homenet.telecomitalia.it>
2024-04-05 18:19:26 +0200

    fix utils

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-96 bt-solid-1-on-background">
+      <span className="">Rights</span>
+      <Link href="/">Wellintone</Link>
+      <Link href="/">Say Hello</Link>
+    </footer>
+  );
 };
 
 export default Footer;
